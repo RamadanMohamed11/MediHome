2309044f6fa99aff525ee5632a76b7bb86299154
Ramadan Mohamed <elking.medo61111@gmail.com>
2025-09-18 02:25:59 +0300

    create signIn and signUp methods

---
+++ lib/core/utils/authentication_service.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:medihome/features/authentication/data/models/user_model.dart';
+
+class AuthenticationService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final CollectionReference _usersCollection = FirebaseFirestore.instance
+      .collection('users');
+
+  void storeUser(UserCredential userCredential, String name, String email) {
+    UserModel user = UserModel(
+      id: userCredential.user!.uid,
+      email: email,
+      name: name,
+    );
+    _usersCollection.doc(user.id).set(user.toJson());
+  }
+
+  Future<void> signUp({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      UserCredential userCredential = await _firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      storeUser(userCredential, name, email);
+    } catch (e) {
+      print("Error signing up: $e");
+    }
+  }
+
+  Future<void> signIn({required String email, required String password}) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (e) {
+      print("Error signing in: $e");
+    }
+  }
+}
